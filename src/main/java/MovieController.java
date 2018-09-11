c0be828ce7e01824cae32fd78da35b17f5f066c3
Alxbnk <Alxbnk@hotmail.com>
2018-09-11 13:06:00 +0200

    DEV2

--- src/main/java/MovieController.java
+++ src/main/java/MovieController.java
@@ -1,2 +1,19 @@
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class MovieController {
+
+    @GetMapping("/")
+    public String Index(){
+        return "Index";
+    }
+
+    @GetMapping("/Hello")
+    public String Hello(){
+        return "Hello";
+    }
+
+    @GetMapping("/Alex")
+    public String Alex(){
+        return "Alex";
+    }
 }
